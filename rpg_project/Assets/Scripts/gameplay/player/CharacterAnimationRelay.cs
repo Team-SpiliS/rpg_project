1c7fccfefebb83f0f81779e5cc52168b80e8b67c
error40404040 <93682132+error40404040@users.noreply.github.com>
2026-06-26 01:43:22 +0300

    small fixes
    поправил некоторые незначительные косяки, а также раскидал все скрипты по папкам для лучшей навигации

---
+++ rpg_project/Assets/Scripts/gameplay/player/CharacterAnimationRelay.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class CharacterAnimationRelay : MonoBehaviour
+{
+    private Animator animator;
+    private HealthComponent health;
+
+    void Awake()
+    {
+        Transform root = transform.root;
+        health = root.GetComponentInChildren<HealthComponent>();
+        animator = root.GetComponentInChildren<Animator>();
+    }
+
+    void OnEnable()
+    {
+        if (health != null)
+        {
+            health.OnTakeDamage += HandleTakeDamage;
+            health.OnDeath += PlayDeathAnimation;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (health != null)
+        {
+            health.OnTakeDamage -= HandleTakeDamage;
+            health.OnDeath -= PlayDeathAnimation;
+        }
+    }
+
+    void HandleTakeDamage(int amount)
+    {
+        if (health != null && health.GetCurrentHealth() <= 0) return;
+
+        PlayHitAnimation();
+    }
+
+    void PlayHitAnimation()
+    {
+        if (animator == null) return;
+
+        if (HasParameter("Hit"))
+        {
+            animator.SetTrigger("Hit");
+        }
+    }
+
+    void PlayDeathAnimation()
+    {
+        if (animator == null) return;
+
+        if (HasParameter("IsDead"))
+        {
+            animator.SetBool("IsDead", true);
+        }
+
+        if (HasParameter("Speed"))
+        {
+            animator.SetFloat("Speed", 0f);
+        }
+    }
+
+    private bool HasParameter(string paramName)
+    {
+        if (animator == null) return false;
+        foreach (AnimatorControllerParameter param in animator.parameters)
+        {
+            if (param.name == paramName) return true;
+        }
+        return false;
+    }
+
+}
